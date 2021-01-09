df2a74c5e7545282763771153cd04b98e851e781
troylochner <troy.lochner@gmail.com>
2021-01-09 09:50:45 -0500

    Updating seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,3 @@
 DROP DATABASE IF EXISTS toolshed;
 CREATE DATABASE tooldshed;
 USE tooldshed
-
-
-INSERT INTO tools (`name`, `description`)
-VALUES('hammer','this is a hammer'),('nail','this is a nail')
-
-INSERT INTO user (`name`, `email`)
-VALUES('troy','troy.lochner@gmail.com')
