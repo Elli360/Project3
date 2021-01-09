e88bf3eab2903691bd6f7435e246f154f6302135
troylochner <troy.lochner@gmail.com>
2021-01-09 11:12:35 -0500

    Updated server files

--- server.js
+++ server.js
@@ -25,10 +25,8 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// Requiring our routes
+// ADDED API ROUTE
 require("./routes/toolshed-api.js")(app);
-
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
