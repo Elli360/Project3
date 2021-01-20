67aab2925145193ad8b28f7b9d8913ef476f16b7
troylochner <troy.lochner@gmail.com>
2021-01-20 17:43:10 -0500

    added callback route

--- client/src/index.js
+++ client/src/index.js
@@ -19,6 +19,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+
+
 import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss?v=1.2.0";
 import "assets/demo/demo.css";
@@ -30,10 +32,14 @@ import RegisterPage from "views/examples/RegisterPage.js";
 import Loaned from "pages/Loaned.js";
 import Borrowed from "pages/Borrowed.js";
 
+//CALLBACK ROUTE TO BE USED BY OKTA
+const CALLBACK_PATH = '/login/callback';
+
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
+      <Route path={CALLBACK_PATH} />
       <Route path="/components" render={(props) => <Index {...props} />} />
       <Route
 
