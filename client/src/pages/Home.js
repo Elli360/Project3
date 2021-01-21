84887a2f6d24672632da228e3bcade2dfd0578e1
shalom dawit <shalom.dawit@gmail.com>
2021-01-20 21:34:05 -0500

    cleaning code for Home.js by adding components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -25,36 +25,17 @@ import ReactDatetime from "react-datetime";
 
 // reactstrap components
 import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  ListGroupItem,
-  ListGroup,
   Container,
   Row,
   Col,
-  FormGroup,
-  UncontrolledTooltip,
-  UncontrolledPopover,
-  PopoverBody,
-  PopoverHeader,
-  Modal,
-  Form,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Label,
-  UncontrolledCarousel,
 } from "reactstrap";
 
 // core components
 import ToolShedNavbar from "components/Navbars/ToolShedNavbar.js";
+import DisplayAllHome from "components/DisplayAllHome/DisplayAllHome.js";
 import AddBtnHome from "components/AddBtnHome/AddBtnHome.js";
-import SearchBtnHome from "componetns/SearchBtnHome/SearchBtnHome.js";
+import SearchBtnHome from "components/SearchBtnHome/SearchBtnHome.js";
+import UserNameDisplay from "components/UserNameDisplay/UserNameDisplay.js";
 import Footer from "components/Footer/Footer.js";
 
 import ToolCard from "components/ToolCard/Toolcard.js"
@@ -62,7 +43,6 @@ import ToolCard from "components/ToolCard/Toolcard.js"
 import bigChartData from "variables/charts.js";
 
 // ====================== post import
-import { Inputs, TextArea, FormBtn } from "../components/Form";
 import API from 'utils/toolshed-api';
 import React, { useEffect, useState } from "react";
 import ExampleToolList from "components/ExampleToolList/ExampleToolList";
@@ -71,8 +51,6 @@ import ExampleToolList from "components/ExampleToolList/ExampleToolList";
 
 export default function Home() {
 
-  const [formModalAdd, setFormModalAdd] = React.useState(false);
-  const [formModalSearch, setFormModalSearch] = React.useState(false);
   React.useEffect(() => {
     document.body.classList.toggle("landing-page");
     // Specify how to clean up after this effect:
@@ -92,50 +70,6 @@ export default function Home() {
   useEffect(() => {
     loadTools()
   }, [])
-
-
-  // Handles updating component state when the user types into the input field
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({ ...formObject, [name]: value })
-  };
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.name && formObject.categoryId) {
-      API.saveTool({
-        name: formObject.name,
-        description: formObject.description,
-        categoryId: formObject.categoryId,
-        price: formObject.price
-        // price: formObject.price,
-        // available:formObject.available
-      })
-        .then(() => setFormObject({
-          name: "",
-          description: "",
-          price: 0,
-          categoryId: 0
-          // price: "",
-          // available:null
-        }))
-        .then(() => loadTools())
-        .catch(err => console.log(err));
-    }
-
-  };
-
-  const [formObject, setFormObject] = useState({
-    name: "",
-    description: "",
-    price: [],
-    categoryId: []
-  });
-
-
-  //==============================================
-
-
 
 
   return (
@@ -186,92 +120,15 @@ export default function Home() {
                   Where you can add, edit and LOAN tools from your inventory.
                   Where you can search for NEW tools and borrow from other users' inventories!...
                 </p>
-                <div className="btn-wrapper mb-3">
-                  <p className="category text-success d-inline">
-                    Open the door to your ToolShed
-                  </p>
-
-                  <Button
-                    className="btn-link"
-                    color="success"
-                    href="#pablo"
-                    onClick={(e) => e.preventDefault()}
-                    size="sm"
-                  >
-                    <i className="tim-icons icon-minimal-right" />
-                  </Button>
-                </div>
+                <DisplayAllHome />
                 <div className="btn-wrapper">
                   <div className="button-container">
 
 
-                <AddBtnHome/>
+                    <AddBtnHome />
 
-                   <SearchBtnHome/>
+                    <SearchBtnHome />
 
-                    {/* Start Search Form Modal */}
-                    {/* <Modal
-                      modalClassName="modal-black"
-                      isOpen={formModalSearch}
-                      toggle={() => setFormModalSearch(false)}
-                    >
-                      <div className="modal-header justify-content-center">
-                        <button className="close" onClick={() => setFormModalSearch(false)}>
-                          <i className="tim-icons icon-simple-remove text-white" />
-                        </button>
-                        <div className="text-muted text-center ml-auto mr-auto">
-                          <h3 className="mb-0">Add to the ToolShed</h3>
-                        </div>
-                      </div>
-                      <div className="modal-body">
-                        <div className="btn-wrapper text-center">
-
-                        </div>
-                        <div className="text-center text-muted mb-4 mt-3">
-                          <small>Describe what you are looking for in your ToolShed</small>
-                        </div>
-                        <div>
-                          {/* ====== mod==================================*/}
-                    {/*  <form>
-                            <Inputs
-                              onChange={handleInputChange}
-                              name="name"
-                              placeholder="name (required)"
-                              value={formObject.name}
-                            />
-                            <Inputs
-                              onChange={handleInputChange}
-                              name="description"
-                              placeholder="description (required)"
-                              value={formObject.description}
-                            />
-                            <Inputs
-                              onChange={handleInputChange}
-                              name="price"
-                              placeholder="price (Optional)"
-                              value={formObject.price}
-                            />
-                            <Inputs
-                              onChange={handleInputChange}
-                              name="categoryId"
-                              placeholder="id (Mandatory)"
-                              value={formObject.categoryId}
-                            />
-
-                            <FormBtn
-                              disabled={!(formObject.name && formObject.categoryId)}
-                              onClick={handleFormSubmit}
-                            >
-                              Submit Tool
-                        </FormBtn>
-                          </form>
-                          {/* ======================================= */}
-                    {/* </div>
-
-
-                      </div>
-                    </Modal> */}
-                    {/* End Search Form Modal */}
                   </div>
 
 
@@ -287,8 +144,10 @@ export default function Home() {
                 />
               </Col>
             </Row>
+
           </div>
         </div>
+        <UserNameDisplay />
       </div>
 
       <div>
@@ -307,8 +166,8 @@ export default function Home() {
             src={require("assets/img/path3.png").default}
           />
           <Container>
-              {/* ToolChest list section */}
-        <ExampleToolList/>
+            {/* ToolChest list section */}
+            <ExampleToolList />
           </Container>
         </section>
 
