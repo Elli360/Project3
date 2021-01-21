ec24d256c305dd69a911a6debfd9432acaba3605
troylochner <troy.lochner@gmail.com>
2021-01-20 19:24:35 -0500

    auth2 latest

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -39,12 +39,8 @@ export default withOktaAuth(class Home extends Component {
       <div className="App">
         <header className="App-header">
          
-          <p>
-            Edit <code>src/Home.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
+       
+          <h1>Welcome to the Toolshed</h1>
           {body}
         </header>
       </div>
