7bdf10aa4ed21733c05ea17ec087f4ed6c536811
troylochner <troy.lochner@gmail.com>
2021-01-23 10:23:53 -0500

    Updated navbar to point to admin login page

--- client/src/components/Navbars/ToolShedNavbar.js
+++ client/src/components/Navbars/ToolShedNavbar.js
@@ -136,7 +136,7 @@ export default function ToolShedNavbar() {
                 className="nav-link d-none d-lg-block"
                 color="primary"
                 target="_blank"
-                href="#Login"
+                href="/admin-login"
               >
                 <i className="tim-icons icon-spaceship" /> Login
 
