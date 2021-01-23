08a1a1762ee15a55b9c9f005ef364aa0d275e746
troylochner <troy.lochner@gmail.com>
2021-01-23 11:46:57 -0500

    Login page looking much cleaner

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 import { withOktaAuth } from '@okta/okta-react';
 
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardImg,
+  CardTitle,
+  Label,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ToolShedNavbar from "components/Navbars/ToolShedNavbar.js";
+import Footer from "components/Footer/Footer.js";
+
 async function checkUser() {
   if (this.props.authState.isAuthenticated && !this.state.userInfo) {
     const userInfo = await this.props.authService.getUser();
@@ -49,10 +74,58 @@ export default withOktaAuth(class Home extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Welcome to the Toolshed</h1>
-          {body}
-        </header>
+        <div className="wrapper">
+
+          <ToolShedNavbar/>
+
+          <div className="wrapper">
+          <div className="page-header">
+
+          <div className="content-center">
+          <Row className="row-grid justify-content-between align-items-center text-left">
+          <Col lg="6" md="6">
+                <h1 className="text-white">
+                  Welcome to the ToolShed! <br />
+                  <span className="text-white"></span>
+                </h1>
+                <p className="text-white mb-3">
+                  Where you can add, edit and LOAN tools from your inventory.
+                  Where you can search for NEW tools and borrow from other users' inventories!...
+                </p>
+               
+                <div className="btn-wrapper">
+                  <div className="button-container">
+                  {body}
+
+                  </div>
+
+
+
+                </div>
+              </Col>
+              <Col lg="4" md="5">
+                <img id="toolshed"
+                  alt="..."
+                  className="img-fluid"
+                  src={require("assets/img/toolshedpicd.jpg").default}
+
+                />
+              </Col>
+              </Row>
+
+          
+
+          </div>
+        
+        <Footer />
+        
+        </div>
+        </div>
+
+
+
+
+        </div>
       </div>
     );
   }
