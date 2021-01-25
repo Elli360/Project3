858b8597832fe310fb076494bf58220c6cde462d
shalom dawit <shalom.dawit@gmail.com>
2021-01-25 01:02:00 -0500

    non-error heroku fix for blank page. 3

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile('client/build', 'index.html');
   });
 }
 
