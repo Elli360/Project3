a45cfc76826bdd075041c022d89d559786778eaa
shalom dawit <shalom.dawit@gmail.com>
2021-07-13 01:58:54 -0400

    testing HashRouter in deployment environment

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //HashRouter during development adds terms to url and redirects incorrectly
-// import { HashRouter as Router } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 // import './index.css';
 // import { App } from './App';
 import App from './App';
