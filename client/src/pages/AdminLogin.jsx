ddb75da325d2a1391d912a27da25251bf55cb345
shalom dawit <shalom.dawit@gmail.com>
2021-07-23 17:23:14 -0400

    cleaned code a little by removing commented out sections

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -28,7 +28,7 @@ export default withOktaAuth(class Home extends Component {
     this.logout = this.logout.bind(this);
     this.state = { userInfo: null };
     this.checkUser = checkUser.bind(this);
-    
+
   }
 
 
@@ -40,13 +40,13 @@ export default withOktaAuth(class Home extends Component {
     await this.props.oktaAuth.signOut();
   }
   render() {
-    
+
     let body = null;
     if (this.props.authState.isAuthenticated) {
       body = (
         <div className="Buttons">
           You are currently logged in... click the button to logout.
-          <button onClick={this.logout}>Logout</button>    
+          <button onClick={this.logout}>Logout</button>
         </div>
       );
     } else {
@@ -61,51 +61,35 @@ export default withOktaAuth(class Home extends Component {
     return (
       <div className="App">
         <div className="wrapper">
-
-    
-
           <div className="wrapper">
-          <div className="page-header">
+            <div className="page-header">
 
-          <div className="content-center">
-          <Row className="row-grid justify-content-between align-items-center text-left">
-          <Col lg="6" md="6">
-                <h1 className="text-white">
-                  Welcome to the ToolShed! <br />
-                  <span className="text-white"></span>
-                </h1>
-                <p className="text-white mb-3">
-                  Where you can add, edit and LOAN tools from your inventory.
-                  Where you can search for NEW tools and borrow from other users' inventories!...
-                </p>
-               
-                <div className="btn-wrapper">
-                  <div className="button-container">
-                  {body}
+              <div className="content-center">
+                <Row className="row-grid justify-content-between align-items-center text-left">
+                  <Col lg="6" md="6">
+                    <h1 className="text-white">
+                      Welcome to the ToolShed! <br />
+                      <span className="text-white"></span>
+                    </h1>
+                    <p className="text-white mb-3">
+                      Where you can add, edit and LOAN tools from your inventory.
+                      Where you can search for NEW tools and borrow from other users' inventories!...
+                    </p>
 
-                  </div>
+                    <div className="btn-wrapper">
+                      <div className="button-container">
+                        {body}
 
+                      </div>
+                    </div>
+                  </Col>
+                  <Col lg="4" md="5">
 
-
-                </div>
-              </Col>
-              <Col lg="4" md="5">
-                
-              </Col>
-              </Row>
-
-          
-
+                  </Col>
+                </Row>
+              </div>
+            </div>
           </div>
-        
-    
-        
-        </div>
-        </div>
-
-
-
-
         </div>
       </div>
     );
--- client/src/pages/Loaned.js
+++ client/src/pages/Loaned.js
@@ -181,117 +181,7 @@ export default function Loaned() {
                         <UncontrolledTooltip delay={0} placement="left" target="tooltip20">
                           Add Loaned Tool from Tool Directory
                         </UncontrolledTooltip>
-                        {/* Start Add Form Modal */}
-                        {/* <Modal
-                          modalClassName="modal-black"
-                          isOpen={formModal}
-                          toggle={() => setFormModal(false)}
-                        >
-                          <div className="modal-header justify-content-center">
-                            <button className="close" onClick={() => setFormModal(false)}>
-                              <i className="tim-icons icon-simple-remove text-white" />
-                            </button>
-                            <div className="text-muted text-center ml-auto mr-auto">
-                              <h3 className="mb-0">Add to Your Loaned Tools</h3>
-                            </div>
-                          </div>
-                          <div className="modal-body">
-                            <div className="btn-wrapper text-center">
-
-                            </div>
-                            <div className="text-center text-muted mb-4 mt-3">
-                              <small>What are the details to this newly loaned tool?</small>
-                            </div>
-                            <Form role="addForm">
-                              <FormGroup className="mb-3">
-                                <InputGroup>
-                                  <InputGroupAddon addonType="prepend">
-                                    <InputGroupText>
-                                      Name:
-                                    </InputGroupText>
-                                  </InputGroupAddon>
-                                  <Input
-                                    placeholder="Tool Name"
-                                    aria-label="Tool Name"
-                                    aria-describedby="basic-addon1"
-                                  />
-                                </InputGroup>
-                              </FormGroup>
-                              <FormGroup>
-                                <InputGroup>
-                                  <InputGroupAddon addonType="prepend">
-                                    <InputGroupText>
-                                      Category:
-                                    </InputGroupText>
-                                  </InputGroupAddon>
-                                  <Input
-                                    placeholder="Tool Category"
-                                    type="text"
-                                  />
-                                </InputGroup>
-                              </FormGroup>
-                              <FormGroup>
-                                <InputGroup>
-                                  <InputGroupAddon addonType="prepend">
-                                    <InputGroupText>
-                                      Description:
-                                    </InputGroupText>
-                                  </InputGroupAddon>
-                                  <Input
-                                    placeholder="Tool Description"
-                                    type="text"
-                                  />
-                                </InputGroup>
-                              </FormGroup>
-                              <FormGroup>
-                                <InputGroup>
-                                  <InputGroupAddon addonType="prepend">
-                                    <InputGroupText>
-                                      Price:
-                                    </InputGroupText>
-                                  </InputGroupAddon>
-                                  <Input
-                                    placeholder="Tool Price"
-                                    type="text"
-                                  />
-                                </InputGroup>
-                              </FormGroup>
-                              <FormGroup>
-                                <InputGroup>
-                                  <InputGroupAddon addonType="prepend">
-                                    <InputGroupText>
-                                      Loaned To:
-                                    </InputGroupText>
-                                  </InputGroupAddon>
-                                  <Input
-                                    placeholder="User Name"
-                                    type="text"
-                                  />
-                                </InputGroup>
-                              </FormGroup>
-                              <FormGroup>
-                                <InputGroup>
-                                  <InputGroupAddon addonType="prepend">
-                                    <InputGroupText>
-                                      Promise Date:
-                                    </InputGroupText>
-                                  </InputGroupAddon>
-                                  <Input
-                                    placeholder="Expected Return Date"
-                                    type="text"
-                                  />
-                                </InputGroup>
-                              </FormGroup>
-
-                              <div className="text-center">
-                                <Button className="my-4" color="primary" type="button">
-                                  ADD TOOL
-                                </Button>
-                              </div>
-                            </Form>
-                          </div>
-                        </Modal> */}
-                        {/* End Add Form Modal */}
+    
                     
                         <Button
                           className="btn-icon btn-simple btn-round btn-neutral"
@@ -446,13 +336,7 @@ export default function Loaned() {
         </div>
 
         <div>
-       {/* <Router>
-         <TestNavbar/> 
-          <NavItem>
-              <NavLink tag={Link} to="/update">
-                Update
-              </NavLink>
-            </NavItem>  */}
+
             <Container>
               <Row>
                 <Col lg="6" md="6"> 
@@ -460,19 +344,7 @@ export default function Loaned() {
             </Col>
             </Row>
             </Container>
-        
-       {/* </Router> */}
-           
-                   {/* <NavItem>
-         <NavLink tag={Link} to="/update">
-           Update
-         </NavLink>
-       </NavItem>
-        <Router>          
-               <div>
 
-               </div>
-</Router> */}
         </div>
 
         {/* Loaned Tools list section */}
@@ -494,128 +366,7 @@ export default function Loaned() {
           <LoanedToolsByCategory />
         }
 
-          {/* <Container>
-            <Row>
-              <Col md="4">
-                <hr className="line-info" />
-                <h1>
-                  Loaned Out{" "}
-                  <span className="text-info">please return as received</span>
-                </h1>
-              </Col>
-            </Row>
-            <Row>
-              <Col md="4">
-                <Card className="card-coin card-plain">
-                  <CardHeader>
-                    <img
-                      alt="..."
-                      className="img-center img-fluid"
-                      src={require("assets/img/mechanic-toolset.jpg").default}
-                    />
-                  </CardHeader>
-                  <CardBody>
-                    <Row>
-                      <Col className="text-center" md="12">
-                        <h4 className="text-uppercase">SAE Wrench set</h4>
-                        <span>Hand Tools</span>
-                        <hr className="line-primary" />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <ListGroup>
-                        {/* <ListGroupItem>Category: {{category}}</ListGroupItem> 
-                        <ListGroupItem>Description</ListGroupItem>
-                        <ListGroupItem>Date Loaned Out</ListGroupItem>
-                        <ListGroupItem>Return Promise</ListGroupItem>
-                      </ListGroup>
-                    </Row>
-                  </CardBody>
-                  <CardFooter className="text-center">
-                    <Button className="btn-simple" color="primary">
-                      Edit
-                    </Button>
-                    <Button className="btn-simple" color="danger">
-                      Remove
-                    </Button>
-                  </CardFooter>
-                </Card>
-              </Col>
-              <Col md="4">
-                <Card className="card-coin card-plain">
-                  <CardHeader>
-                    <img
-                      alt="..."
-                      className="img-center img-fluid"
-                      src={require("assets/img/protective-gear.jpg").default}
-                    />
-                  </CardHeader>
-                  <CardBody>
-                    <Row>
-                      <Col className="text-center" md="12">
-                        <h4 className="text-uppercase">Welder Gloves</h4>
-                        <span>Protective Gear</span>
-                        <hr className="line-success" />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <ListGroup>
-                        {/* <ListGroupItem>Category: {{category}}</ListGroupItem> 
-                        <ListGroupItem>Description</ListGroupItem>
-                        <ListGroupItem>Date Loaned Out</ListGroupItem>
-                        <ListGroupItem>Return Promise</ListGroupItem>
-                      </ListGroup>
-                    </Row>
-                  </CardBody>
-                  <CardFooter className="text-center">
-                    <Button className="btn-simple" color="success">
-                      Edit
-                    </Button>
-                    <Button className="btn-simple" color="danger">
-                      Remove
-                    </Button>
-                  </CardFooter>
-                </Card>
-              </Col>
-              <Col md="4">
-                <Card className="card-coin card-plain">
-                  <CardHeader>
-                    <img
-                      alt="..."
-                      className="img-center img-fluid"
-                      src={require("assets/img/power-tools.jpg").default}
-                    />
-                  </CardHeader>
-                  <CardBody>
-                    <Row>
-                      <Col className="text-center" md="12">
-                        <h4 className="text-uppercase">Impact Wrench</h4>
-                        <span>Power Tools</span>
-                        <hr className="line-info" />
-                      </Col>
-                    </Row>
-                    <Row>
-                      <ListGroup>
-                        {/* <ListGroupItem>Category: {{category}}</ListGroupItem> 
-                        <ListGroupItem>Description</ListGroupItem>
-                        <ListGroupItem>Date Loaned Out</ListGroupItem>
-                        <ListGroupItem>Return Promise</ListGroupItem>
-                      </ListGroup>
-                    </Row>
-                  </CardBody>
-                  <CardFooter className="text-center">
-                    <Button className="btn-simple" color="info">
-                      Edit
-                    </Button>
-                    <Button className="btn-simple" color="danger">
-                      Remove
-                    </Button>
-                  </CardFooter>
-                </Card>
-              </Col>
-            </Row>
-          </Container> */}
-
+  
 
         </section>
 
