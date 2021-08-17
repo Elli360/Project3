098033cee47733e5ff882fb245aa731a27be0816
shalom dawit <shalom.dawit@gmail.com>
2021-08-16 22:30:50 -0400

    cleaning code for LoanedHardware helped alot, next step is adding a counter hook to display 'no tools available'

--- client/src/components/LoanedHardware/index.js
+++ client/src/components/LoanedHardware/index.js
@@ -1,15 +1,9 @@
 
 import React, { useEffect, useState } from "react";
 import api from "../../utils/api";
-// const delay = 2;
-//make category.id # a prop so the # of components can be divided by 5
+
 function LoanedHardware() {
-  // const [show, setShow] = useState(false);
-  // const [categories, setCategories] = useState([]);
   const [categories, setCategories] = useState([]);
-
-
-
   useEffect(() => {
 
     const loadTools = async () => {
@@ -24,169 +18,67 @@ function LoanedHardware() {
 
     loadTools()
 
-    // if (!categories) {
-    //   loadTools()
-    // }
-
-
-    //        let timer1 = setTimeout(() => setShow(true), delay * 1000);
-
-    //     // this will clear Timeout
-    //     // when component unmount like in willComponentUnmount
-    //     // and show will not change to true
-    //     return () => {
-    //       loadTools()
-    //       clearTimeout(timer1);
-    //     };
   },
-    // // useEffect will run only one time with empty []
-    // // if you pass a value to array,
-    // // like this - [data]
-    // // than clearTimeout will run every time
-    // // this value changes (useEffect re-run)
-
-    // []);
     []);
 
-  // function loadTools() {
-  //   api.getCategories().then(res => setCategories(res.data)).then((categories)=>console.log(`LOVE ${categories[1]} PATH`))
-  // };
-
-  // const loadTools = async () => {
-
-  //   const first = await api.getCategories();
-  //   const res = await first.data[4].tools;
-  //   // console.log(`CONSOLE CHECK : ${res[3].id}`);
-  //   console.log(`CONSOLE CHECK : ${res[3].id}`);
-  //   console.log(toolsAvailable)
-  //   setCategories(res);
-  // }
-  // console.log(categories);
-  // console.log(`HELLO WORLD=${categories[0].name}`)
-  const toad = `TOAD`
   const toolsAvailable = categories && categories.map(tool => {
-    if (tool.available === true) {
+    if (tool.available === false) {
       return (
-        // <ul>
-        //   <li key={tool.id}>{tool.name}</li>
-        // </ul>
-        <ul>ToolsWork</ul>
+        <ul>
+          <li key={tool.id}>{tool.name}</li>
+        </ul>
+        // <ul>ToolsWork</ul>
       )
     }
     else {
-      return <ul>ToolsWork</ul>
-      // return false
+      return (
+      // <ul><li>Tool Not Available</li></ul>
+      null
+      )
     }
   });
 
-  //  const toolsTrue=()=> { return (<div>{toolsAvailable }</div>)}
-  //   const toolsFalse =()=>{return (<div>No more toolz</div>)}  
+  // const ToolsConditional = () => {
+  //   // const toolsTrue = () => { return (<div>{toolsAvailable}</div>) }
+  //   const toolsFalse = () => { return (<div>No more toolz</div>) }
+  //   if (categories) {
+  //     //  const renderTools = <toolsTrue/>
+  //     // const renderTools = <toolsTrue/>;
 
-  const ToolsConditional = () => {
-    // const toolsTrue = () => { return (<div>{toolsAvailable}</div>) }
-    const toolsFalse = () => { return (<div>No more toolz</div>) }
-    if (categories) {
-      //  const renderTools = <toolsTrue/>
-      // const renderTools = <toolsTrue/>;
+  //     return (
+  //       // console.log(`Because I'm SAD!`),
+  //       // renderTools
 
-      return (
-        // console.log(`Because I'm SAD!`),
-        // renderTools
-
-        // {toolsTrue}
-      <div>It Worked? </div>
-        )
-    }
-    else {
-      // const renderToolz = <toolsFalse/>;
-      return (
-        // console.log(`Because I'm HAPPY!`),
-        // renderToolz   
-        {toolsFalse}
-      )
-    }
-  }
-  //  const ToolConditional =()=> {
-
-  //       if (toolsAvailable) {
-  //         return { toolsAvailable }
-  //       }
-  //       return (<div>No more toolz</div>)
+  //       // {toolsTrue}
+  //     <div>It Worked? </div>
+  //       )
   //   }
+  //   else {
+  //     return (  
+  //       {toolsFalse}
+  //     )
+  //   }
+  // }
 
-  // console.log(`FIRST CHECK THIS: ${ToolConditional}`);
   return (
 
-    //   {if (toolsAvailable) {
-    //     const renderTools = <toolsTrue/>
-    //  }, 
-    // //  else {const renderNoTools = <toolsFalse/>}}
-    // if(!toolsAvailable) {const renderNoTools = <toolsFalse/>}}
-    // <div className="toolDetail" >
     <div>
-      {ToolsConditional}
-      {/* {renderTools}
-   {renderNoTools} */}
+      {toolsAvailable}
       {console.log(`Checking toolsAvailable: ${toolsAvailable} and ToolConditional:${toolsAvailable}`)}
-      {console.log(`Checking toolsAvailable: ${toad} and ToolConditional:${toad}`)}
+  
 
     </div>
-    //works sans the <div>NO tools</div>
-    // <div>
-    //   {categories.length ? (
-    //     <ul>
-    //       {categories.map(category => {
-    //         if (category.id === 5) {
-    //           // const hardwareAnswer = category
-    //           return (
-    //             <div key={category.id}>
-    //               {/* <a href={"/tools/" + category.id}>{category.name}</a> */}
+    
 
-    //               <ul>
+  )
+}
 
-    //                 <div className="toolDetail" >
-    //                   {/* {hardwareAnswer.length?.map(tool => { */}
-    //                     {category.tools.map(tool => {
-
-    //                     if (tool.available === false) {
-    //                       return (
-    //                         <li key={tool.id}>{tool.name}{console.log(tool.name)}</li>
-    //                       )
-    //                     }
-    //                     else {
-    //                       return null
-    //                     }
-    //                   })}
-
-    //                 </div>
-    //               </ul>
-    //             </div>
-
-    //           );
-    //         }
-    //         else {
-    //           return null
-    //         }
-    //       }
-
-    //       )}
-    //     </ul>
-    //   ) : (
-    //     // show ? (
-    //     //   <h3>No Tools were found.</h3>
-
-    //     //   ) : (
-    //         <div>No Tools were found.</div>
-    //       // )
-
-    //   )}
-
-    // </div>
+export default LoanedHardware;
 
 
 
 
-    //simplied plus works sans the <div>NO tools</div>
+//simplied plus works sans the <div>NO tools</div>
     // <div>
     //   {categories.length ? (
     //     <ul>
@@ -209,15 +101,3 @@ function LoanedHardware() {
     //   )
     //   }
     // </div>
-
-
-
-    //simplied sans the <div>NO tools</div>
-    // <div className="toolDetail" >
-
-
-
-  )
-}
-
-export default LoanedHardware;
