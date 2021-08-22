6489d070bfb8c2b80109dea4e20c5fb0ecf91ce6
shalom dawit <shalom.dawit@gmail.com>
2021-08-21 23:59:36 -0400

    added filter to LoanedHardware instead of axios query change

--- client/src/components/LoanedHardware/index.js
+++ client/src/components/LoanedHardware/index.js
@@ -3,79 +3,77 @@ import React, { useEffect, useState } from "react";
 import api from "../../utils/api";
 
 function LoanedHardware() {
+  // const [count, setCount] = useState(0);
   const [categories, setCategories] = useState([]);
-  const [count, setCount] = useState(0);
+  
+
+  const loadTools = async () => {
+
+    const first = await api.getCategories();
+    const res = await first.data[4].tools;
+    // console.log(`CONSOLE CHECK : ${res[3].id}`);
+    // console.log(...toolsAvailable, `facts`)
+    setCategories(res);
+  
+  }
+
+
     useEffect(() => {
 
-    const loadTools = async () => {
-
-      const first = await api.getCategories();
-      const res = await first.data[4].tools;
-    
-      
-      // console.log(`CONSOLE CHECK : ${res[3].id}`);
-      console.log(`CONSOLE CHECK : ${res[3].id}`);
-      // console.log(...toolsAvailable, `facts`)
-      setCategories(res);
-    
-    }
+ 
 
     loadTools()
-   
+    // }) 
   },
     []);
-    
-  
-  const toolsAvailable = categories && categories.map((tool, index) => {
-    if (tool.available === false) {
-      setCount(count+1);
+    const nopeTools =(<div>No more tool3</div>)
+    // let notAvailable=false
+  const toolsAvailable =  categories.filter((val)=>{
+    if (val.available === false){return val}else {return (
+    // notAvailable=true
+    null
+
+    )}
+  }).map((tool,index) => {
+
+    // if (tool.available === false) {
+      // setCount(prevState => (prevState+1))
+      // setCount(count+1)
       return (
         <ul>
-          <li key={tool.id}>{tool.name}{index}</li>
+          <li key={tool.id}>{tool.name} index={index}</li>
         </ul>
         // <ul>ToolsWork</ul>
       )
-    }
-    else {
-      return (
-      // <ul><li>Tool Not Available</li></ul>
-      null
-      )
-    }
+    // }
+    // else {
+    //   return (
+    //   // <ul><li>Tool Not Available</li></ul>
+    //   null
+    //   )
+    // }
   });
 
-  // const ToolsConditional = () => {
-  //   // const toolsTrue = () => { return (<div>{toolsAvailable}</div>) }
-    const toolsFalse = () => { return (<div>No more toolz</div>) }
-  //   if (categories) {
-  //     //  const renderTools = <toolsTrue/>
-  //     // const renderTools = <toolsTrue/>;
+  // const nopeTools =()=>{ if(notAvailable){return(<div>No more tool3</div>)}}
 
-  //     return (
-  //       // console.log(`Because I'm SAD!`),
-  //       // renderTools
-
-  //       // {toolsTrue}
-  //     <div>It Worked? </div>
-  //       )
-  //   }
-  //   else {
-  //     return (  
-  //       {toolsFalse}
-  //     )
-  //   }
-  // }
-  if (count === 0){
-    return toolsFalse
-  }
+//   // if (count === 0){
+//   //   return (<div>No more toolz</div>)
+//   // }
+// if( !categories.available){
+//   return(nopeTools)
+// }
+const notAvailable = async() => {
+const checkThis=await categories.filter()
+if (checkThis===null){return nopeTools}
+}
   
   return (
 
     <div>
-      {toolsAvailable}
-      {console.log(`Checking toolsAvailable: ${toolsAvailable} and ToolConditional:${toolsAvailable}`)}
-  
-
+      {toolsAvailable }
+     
+  {/* {nopeTools} */}
+{notAvailable}
     </div>
     
 
