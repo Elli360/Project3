9f459a1a34c8c80f50da7ed0750330106ab5c398
shalom dawit <shalom.dawit@gmail.com>
2021-08-25 11:56:20 -0400

    for LoanedHardware: added a comment and cleaned previous comments

--- client/src/components/LoanedHardware/index.js
+++ client/src/components/LoanedHardware/index.js
@@ -3,42 +3,29 @@ import React, { useEffect, useState } from "react";
 import api from "../../utils/api";
 
 function LoanedHardware() {
-  // const [count, setCount] = useState(0);
+
   const [categories, setCategories] = useState([]);
 
-
   const loadTools = async () => {
-
     const first = await api.getCategories();
     const res = await first.data[4].tools;
-    // console.log(`CONSOLE CHECK : ${res[3].id}`);
-    // console.log(...toolsAvailable, `facts`)
     setCategories(res);
-
   }
 
-
   useEffect(() => {
-
     loadTools()
-
   },
     []);
-  // const nopeTools =(<div>No more tool3</div>)
-  const NopeTools = () => { return (<h3>No Available Tools</h3>) }
-  // let notAvailable=false
+
+  const NoTools = () => { return (<h3>No Available Tools</h3>) }
+
   const toolsAvailable = categories.filter((row) => {
     if (row.available === false) { return row } else {
       return (
-        // notAvailable=true
         null
       )
     }
   }).map((tool) => {
-
-    // if (tool.available === false) {
-    // setCount(prevState => (prevState+1))
-    // setCount(count+1)
     return (
       <div className="toolDetail" >
         <ul>
@@ -46,81 +33,23 @@ function LoanedHardware() {
         </ul>
       </div>
     )
-    // }
-    // else {
-    //   return (
-    //   // <ul><li>Tool Not Available</li></ul>
-    //   null
-    //   )
-    // }
   })
 
-
-  // const nopeTools =()=>{ if(notAvailable){return(<div>No more tool3</div>)}}
-
-  //   // if (count === 0){
-  //   //   return (<div>No more toolz</div>)
-  //   // }
-  // if( !categories.available){
-  //   return(nopeTools)
-  // }
-
-  // const notAvailable = () => {
-  //   return function(){return(toolsAvailable)}.then(console.log(`toolsAvailable:${toolsAvailable}`)).then(nopeTools)
-  //   }
-  // const notAvailable = async() => {
-  // const checkThis=await toolsAvailable
-  // // console.log(`CHECKTHIS: ${checkThis}`)
-  // console.log(`CHECKTHIS:`)
-  // if (checkThis===null){return (
-  //   <>
-  //   {nopeTools}
-  //   </>
-  // )}
-  // }
   const DefaultNotAvailable = () => {
     if (toolsAvailable == "")
-      return NopeTools();
+      return NoTools();
 
     return null;
   }
 
   return (
-
-    <div>
+    <>
       {toolsAvailable}
-      <DefaultNotAvailable />
-    </div>
-
-
+      {DefaultNotAvailable()}
+      {/* also works a component */}
+      {/* <DefaultNotAvailable/> */}
+    </>
   )
 }
 
 export default LoanedHardware;
-
-
-
-
-//simplied plus works sans the <div>NO tools</div>
-    // <div>
-    //   {categories.length ? (
-    //     <ul>
-    //       <div className="toolDetail" >
-    //         {categories.map(tool => {
-    //           if (tool.available === false) {
-    //             return (
-    //               <li key={tool.id}>{tool.name}{console.log(tool.available)}</li>
-    //             )
-    //           }
-    //           else {
-    //             return null
-    //           }
-    //         })}
-    //       </div>
-    //     </ul>
-    //   ) : (
-
-    //     <div>No toolz</div>
-    //   )
-    //   }
-    // </div>
