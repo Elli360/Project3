21f7332e95ef32093956f4882a9fd125eb5f5bad
shalom dawit <shalom.dawit@gmail.com>
2021-09-06 18:24:47 -0400

    fully working category with default value executing AFTER  data is fetched (shown in LoanedHardware component)

--- client/src/components/LoanedHardware/index.js
+++ client/src/components/LoanedHardware/index.js
@@ -5,54 +5,74 @@ import api from "../../utils/api";
 function LoanedHardware() {
 
   const [categories, setCategories] = useState([]);
+  const [categoryTrue, setCategoryTrue] = useState(false);
   const LoadLoanableTools = async () => {
     const first = await api.getCategories();
     const res = await first.data[4].tools;
     setCategories(res);
-  }
+  };
 
   useEffect(() => {
     LoadLoanableTools()
   }, []);
 
-  const NoLoanableTools = () => { return (<h3>All Tools are in the ToolShed &there4; Available</h3>) }
+  const NoLoanableTools = () => { return (<h3>All Tools are in the ToolShed &there4; Available</h3>) };
 
-  const ToolsNotAvailable = () => categories.filter((row) => {
-    if (row.available === false) { return row } else {
+  const ToolsNotAvailable = () =>
+    categories.filter((row) => {
+      if (row.available === false) { return row } else {
+        return (
+          null
+        )
+      }
+    }).map((tool) => {
       return (
-        null
+        <div className="toolDetail" >
+          <ul>
+            <li key={tool.id}>{tool.name}</li>
+          </ul>
+        </div>
       )
-    }
-  }).map((tool) => {
-    return (
-      <div className="toolDetail" >
-        <ul>
-          <li key={tool.id}>{tool.name}</li>
-        </ul>
-      </div>
-    )
-  })
+    });
+  // setCategoryTrue(true);
 
+  // setCategoryTrue(true);
   const DefaultNotAvailable = () => {
     if (ToolsNotAvailable() == "")
       return NoLoanableTools();
-
+    // setCategoryTrue(true);
     return null;
-  }
+  };
   //Does not affect component at all or in desired way
-  // if (!ToolsNotAvailable()) {
-  //   return (
-  //     <div>
-  //       <p>Fetching tool availablility...</p>
-  //     </div>
-  //   );
-  // }
+
+
+  // useEffect(() => {
+    
+  //   const Timeout = () => {
+  //     if (!categoryTrue) {
+  //       return (
+  //         <div>
+  //           <p>Fetching tool availablility...</p>
+  //         </div>
+  //       )
+  //     }
+  //   }
+  //   Timeout();
+  // },[]);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        setCategoryTrue(true);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       {ToolsNotAvailable()}
-      {ToolsNotAvailable() && <DefaultNotAvailable/>}
+      {/* {ToolsNotAvailable() && <DefaultNotAvailable />} */}
+      {categoryTrue && <DefaultNotAvailable />}
     </>
   )
-}
+};
 
 export default LoanedHardware;
