4343671588e397a978f963e8d99e8018cca01c31
shalom dawit <shalom.dawit@gmail.com>
2021-12-02 12:01:10 -0500

    PowerTools: updated modal style

--- client/src/components/PowerTools/index.js
+++ client/src/components/PowerTools/index.js
@@ -5,12 +5,12 @@ import { Button, Modal } from "reactstrap";
 function PowerTools() {
   const [categories, setCategories] = useState([]);
   const [toolModal, setToolModal] = useState(false);
-  const [toolModalData, setToolModalData] = useState(null);
+  const [toolModalData, setToolModalData] = useState([]);
   const [modalReady, setModalReady] = useState(false);
 
   useEffect(() => {
     loadTools();
-    if(!toolModalData === false){
+    if (!toolModalData === false) {
       setModalReady(true);
     }
   }, [toolModalData]);
@@ -36,39 +36,50 @@ function PowerTools() {
                         {category.tools.map((tool) => {
                           return (
                             <li key={tool.id}>
-                              <Button onClick={() =>{
-                                setToolModalData(tool);
-                                setToolModal(true);
-                              }}>
+                              <Button
+                                onClick={() => {
+                                  setToolModalData(tool);
+                                  setToolModal(true);
+                                }}
+                              >
                                 {" "}
                                 {tool.name}
                               </Button>
                             </li>
                           );
                         })}
-                        {modalReady &&
-                               <Modal
-                                modalClassName="modal-black"
-                                isOpen={toolModal}
-                                toggle={() => setToolModal(false)}
+                        {modalReady && (
+                          <Modal
+                            modalClassName="modal-black"
+                            isOpen={toolModal}
+                            toggle={() => setToolModal(false)}
+                          >
+                            <div className="modal-header justify-content-center toolInfoModal">
+                              <button
+                                className="close"
+                                onClick={() => setToolModal(false)}
                               >
-                                <div className="modal-header justify-content-center toolInfoModal">
-                                  <button
-                                    className="close"
-                                    onClick={() => setToolModal(false)}
-                                  >
-                                    <i className="tim-icons icon-simple-remove text-white" />
-                                  </button>
-                                  <div className="text-muted text-center ml-auto mr-auto ">
-                                    <h2 >Tool Info</h2>
-                                    <h5 className="mb-0">Tool Name: {toolModalData.name}</h5>
-                                    <div>Tool Description: {toolModalData.description}</div>
-                                    <div>Price: ${toolModalData.price}</div>
-                                  </div>
+                                <i className="tim-icons icon-simple-remove text-white" />
+                              </button>
+                              <div className="text-muted text-center ml-auto mr-auto ">
+                                <h2>Tool Info</h2>
+                                <h5 className="mb-0">{toolModalData.name}</h5>
+                                <div>
+                                  Description: {toolModalData.description}
                                 </div>
-                              </Modal>}
+                                <div>Price: ${toolModalData.price}</div>
+                                {/* <div>------</div> */}
+                                <br />
+                                {/* <div>Loaned Out: {toolModalData.available.toString()}</div>
+                                <div>Borrowed In: {toolModalData.borrowed.toString()}</div>
+                                <div>Added Time: {toolModalData.createdAt.toString()}</div> */}
+                                <div>Loaned Out: {toolModalData.available}</div>
+                                <div>Borrowed In: {toolModalData.borrowed}</div>
+                              </div>
+                            </div>
+                          </Modal>
+                        )}
                       </div>
-               
                     </ul>
                   </div>
                 );
