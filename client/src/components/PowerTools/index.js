7b8365db10d0553a263b67c7e74df23f8f23800d
shalom dawit <shalom.dawit@gmail.com>
2021-12-09 17:30:32 -0500

    demo.css: edited .page-header height to fix style

--- client/src/components/PowerTools/index.js
+++ client/src/components/PowerTools/index.js
@@ -77,41 +77,41 @@ function PowerTools() {
                         {modalReady && (
 
 
- <ToolModalInfo toolModalOpen={toolModal} toolModalClose={()=>setToolModal(false)} toolObject={toolModalData}/> 
-                          // <Modal className="toolInfoModal"
-                          //   modalClassName="modal-black"
-                          //   isOpen={toolModal}
-                          //   toggle={() => setToolModal(false)}
-                          // >
-                          //   <div className="modal-header justify-content-center">
-                          //     <button
-                          //       className="close"
-                          //       onClick={() => setToolModal(false)}
-                          //     >
-                          //       <i className="tim-icons icon-simple-remove text-white" />
-                          //     </button>
-                          //     <div className="text-muted text-center ml-auto mr-auto ">
-                          //       <h2><span>Tool Info</span></h2>
-                          //       <h5 className="mb-0">{toolModalData.name}</h5>
-                          //       <div>
-                          //         <span>Description:</span> {toolModalData.description}
-                          //       </div>
-                          //       <div><span>Price:</span> ${toolModalData.price}</div>
+//  <ToolModalInfo toolModalOpen={toolModal} toolModalClose={()=>setToolModal(false)} toolObject={toolModalData}/> 
+                          <Modal className="toolInfoModal"
+                            modalClassName="modal-black"
+                            isOpen={toolModal}
+                            toggle={() => setToolModal(false)}
+                          >
+                            <div className="modal-header justify-content-center">
+                              <button
+                                className="close"
+                                onClick={() => setToolModal(false)}
+                              >
+                                <i className="tim-icons icon-simple-remove text-white" />
+                              </button>
+                              <div className="text-muted text-center ml-auto mr-auto ">
+                                <h2><span>Tool Info</span></h2>
+                                <h5 className="mb-0">{toolModalData.name}</h5>
+                                <div>
+                                  <span>Description:</span> {toolModalData.description}
+                                </div>
+                                <div><span>Price:</span> ${toolModalData.price}</div>
 
-                          //       <br />
+                                <br />
                             
-                          //       <div><span>In the ToolShed:</span> {toolModalData.available.toString()}</div>
-                          //       <div><span>Borrowed In:</span> {toolModalData.borrowed.toString()}</div>
+                                <div><span>In the ToolShed:</span> {toolModalData.available.toString()}</div>
+                                <div><span>Borrowed In:</span> {toolModalData.borrowed.toString()}</div>
 
-                          //       {/* first add different timestamp query then try to alter useState type */}
-                          //       {/* <div><span>Added Time:</span> {console.log(toolModalData)}</div>
-                          //       <div>TIME0: {toolModalData.createdAt.toString()}</div>
-                          //       <div><span>TIME1:</span> {new Date(`${toolModalDataCreatedAt}`).toLocaleString()}</div>
-                          //       <div><span>TIME2:</span> {new Date(toolModalData.createdAt.seconds * 1000).toLocaleDateString("en-US")}</div>
-                          //       <div><span>TIME3:</span> {new Date(`${toolModalData.createdAt}`).toLocaleDateString()}</div> */}
-                          //     </div>
-                          //   </div>
-                          // </Modal>
+                                {/* first add different timestamp query then try to alter useState type */}
+                                {/* <div><span>Added Time:</span> {console.log(toolModalData)}</div>
+                                <div>TIME0: {toolModalData.createdAt.toString()}</div>
+                                <div><span>TIME1:</span> {new Date(`${toolModalDataCreatedAt}`).toLocaleString()}</div>
+                                <div><span>TIME2:</span> {new Date(toolModalData.createdAt.seconds * 1000).toLocaleDateString("en-US")}</div>
+                                <div><span>TIME3:</span> {new Date(`${toolModalData.createdAt}`).toLocaleDateString()}</div> */}
+                              </div>
+                            </div>
+                          </Modal>
                         )}
                       </div>
                     </ul>
