3e226231b94d745dce4b7a97f377e7bb285018a0
Shalom Dawit <Shalom.Dawit@gmail.com>
2024-07-19 20:43:57 -0400

    removed comments

--- client/src/components/AllToolsByCategory/index.js
+++ client/src/components/AllToolsByCategory/index.js
@@ -6,24 +6,7 @@ import {
   CardFooter,
   Container,
   Row,
-  Col,
-  // Button,
-  // CardTitle,
-  // ListGroupItem,
-  // ListGroup,
-  // FormGroup,
-  // UncontrolledTooltip,
-  // UncontrolledPopover,
-  // PopoverBody,
-  // PopoverHeader,
-  // Modal,
-  // Form,
-  // Input,
-  // InputGroup,
-  // InputGroupAddon,
-  // InputGroupText,
-  // Label,
-  // UncontrolledCarousel,
+  Col
 } from "reactstrap";
 
 import React from "react";
@@ -32,13 +15,6 @@ import { Link, useLocation } from "react-router-dom";
 import {HashRouter as Router, Route } from "react-router-dom";
 
 import ToolShedByCategory from "../ToolListByCategory/ToolShedByCategory";
-//import HandTools from "../HandTools";
-//import OutdoorTools from "../OutdoorTools";
-//import WorkshopTools from "../WorkshopTools";
-//import Hardware from "../Hardware"
-
-
-
 
 function AllToolsByCategory(props) {
     const location = useLocation();
@@ -280,46 +256,7 @@ function AllToolsByCategory(props) {
                 </CardFooter>
               </Card>
             </Col>
-
-            {/* ==========================category template */}
-            {/* <Col md="4">
-              <Card className="card-coin card-plain">
-                <CardHeader>
-                  <img
-                    alt="..."
-                    className="img-center img-fluid"
-                    src={require("assets/img/power-tools.jpg").default}
-                  />
-                </CardHeader>
-                <CardBody>
-                  <Row>
-                    <Col className="text-center" md="12">
-                      <h4 className="text-uppercase">Hardware</h4>
-                      <span>Power Tools</span>
-                      <hr className="line-info" />
-                    </Col>
-                  </Row>
-                  <Row>
-                    <ListGroup> */}
-                      {/* <ListGroupItem>Category: {{category}}</ListGroupItem> */}
-                      {/* <ListGroupItem>Description</ListGroupItem>
-                      <ListGroupItem>Price</ListGroupItem>
-                      <ListGroupItem>Available?</ListGroupItem>
-                    </ListGroup>
-                  </Row>
-                </CardBody>
-                <CardFooter className="text-center">
-                  <Button className="btn-simple" color="info">
-                    Edit
-                  </Button>
-                  <Button className="btn-simple" color="danger">
-                    Remove from ToolShed
-                  </Button>
-                </CardFooter>
-              </Card>
-            </Col>
- */}
-
+            
           </Row>
           </Router>
         </Container>
