5a4c42b5d6cf6c5b40911324fda6beb43030a975
Shalom Dawit <Shalom.Dawit@gmail.com>
2024-07-29 17:08:55 -0400

    updated package.json files for render deployment (working install of both directories)

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -34,10 +35,21 @@ require("./routes/toolshed-api.js")(app);
   app.use(express.static('client/build'));
   // Handle React routing, return all requests to React app
   app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+
+    console.log("server in production mode");
     // res.sendFile('client/build', 'index.html');
   });
 }; 
+
+// if (process.env.NODE_ENV === 'production') {
+//   // Serve any static files
+//   app.use(express.static('client/build'));
+// // Handle React routing, return all requests to React app
+//   app.get('/', function(req, res) {
+//     res.sendFile('client/build', 'index.html');
+//   });
+// }
 
 
 // set port, listen for requests
