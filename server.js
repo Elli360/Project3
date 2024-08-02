8f51e42f43fecb2c105d0be4263e6405d3f4d698
Shalom Dawit <Shalom.Dawit@gmail.com>
2024-08-02 19:36:41 -0400

    edited server.js and changed env var PORT to 3000

--- server.js
+++ server.js
@@ -31,26 +31,32 @@ require("./routes/toolshed-api.js")(app);
 
 //CONFIGURE FOR HEROKU DEPLoYMENT
 
- if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static('client/build'));
-  // Handle React routing, return all requests to React app
-  app.get('/*',cors(corsOptions), function(req, res) {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-
-    console.log("server in production mode");
-    // res.sendFile('client/build', 'index.html');
-  });
-}; 
-
-// if (process.env.NODE_ENV === 'production') {
+//  if (process.env.NODE_ENV === 'production') {
 //   // Serve any static files
 //   app.use(express.static('client/build'));
-// // Handle React routing, return all requests to React app
-//   app.get('/', function(req, res) {
-//     res.sendFile('client/build', 'index.html');
+//   // Handle React routing, return all requests to React app
+//   app.get('/*',cors(corsOptions), function(req, res) {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+
+//     console.log("server in production mode");
+//     // res.sendFile('client/build', 'index.html');
 //   });
-// }
+// }; 
+
+
+//Configured to work with an already built React static site (found in client/build)
+//After localhost:3000 is started up, Okta can communicate with address along with 
+//DB being called from 1ocalhost:3000/api root
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static('client/build'));
+// Handle React routing, return all requests to React app
+  app.get('/', function(req, res) {
+    res.sendFile('client/build', 'index.html');
+  });
+  console.log("server in production mode");
+}
+
 
 
 // set port, listen for requests
